f3550b2832379e164bbbcfa74d2a6c73f903780d
Shveytser-A <Shveytser-A@gaz-is.ru>
2021-04-17 20:00:34 +0300

    create hw2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
-
+        <div className={s.messageWrapper}>
+            <div className={s.message}>
+                <img alt={'userIcon'} src={props.avatar}/>
+            </div>
+            <div className={s.box}>
+                <div className={s.userName}>{props.name}:</div>
+                <div>{props.message}</div>
+                <div>{props.time}</div>
+            </div>
         </div>
     )
-}
+};
 
 export default Message
